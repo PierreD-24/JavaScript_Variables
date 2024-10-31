5cbc365cafc6d2dd09ab5262495bf95dc863e696
Pierre Dahrouj <pdahrouj7@gmail.com>
2024-10-31 13:20:00 -0700

    completed javascript variable

--- app.js
+++ app.js
@@ -4,7 +4,41 @@ console.log(
 );
 
 // Exercise 1
+const firstName = 'Pierre';
+let lastName = 'Dahrouj';
+var age = 28;
+
+console.log(`Name: ${firstName} ${lastName} Age: ${age}`);
 
 // Exercise 2
+lastName = "";
+age = 29;
 
 // Exercise 3
+let language = 'JavaScript';
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log(`${language} was released in ${createdYear}.`);
+
+// Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize ="M";
+
+console.log(`The price is ${price} it is discounted ${salePercentage} percent.`);
+console.log(`There are currently ${stock} in stock in size ${selectedSize}.`);
+
+// Exercise5
+let title = 'Name of the Wind';
+let author = 'Patrick Rothfuss';
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(`The name of the book is ${title} the author is ${author}.`);
+console.log(`There are ${pageCount} pages, my bookmark is on page ${bookmark}.`);
+
